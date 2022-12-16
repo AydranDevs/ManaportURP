7fc83599396f9afc20a8735a4c722c94c532cd2d
Aydranna Gwendalyn Frost-Walker <71469253+AydranDevs@users.noreply.github.com>
2022-12-16 13:07:11 -0500

    Desktop Transfer (broke rn)

--- Assets/Scripts/Camera/PartyCam.cs
+++ Assets/Scripts/Camera/PartyCam.cs
@@ -11,20 +11,10 @@ namespace Manapotion.PartySystem.Cam
     {
         public PartyCameraManagerScriptableObject partyCameraManager;
 
-        public class CameraState
-        {
-            /// <summary>
-            /// the camera will find a center point between these transforms if targets.Length > 0
-            /// </summary>
-            public Transform[] targets;
-        }
-        private CameraState _cameraState;
-
         public CamZoomState camZoomState = CamZoomState.ZoomedOut;
 
         private const float DEFAULT_CAMERA_SIZE = 7.3125f;
 
-        public float speed = 1f;
         public Transform target;
         private Vector3 _target;
 
@@ -33,11 +23,6 @@ namespace Manapotion.PartySystem.Cam
         bool transitionOver = false;
 
         public float transitionDuration = 2.5f;
-
-        public void SetCameraState(CameraState cameraState)
-        {
-            _cameraState = cameraState;
-        }
         
         void Start() {
             cam = GetComponent<Camera>();
@@ -46,39 +31,44 @@ namespace Manapotion.PartySystem.Cam
             StartCoroutine(Transition());
         }
 
-        void Update() {
-            _target = new Vector3(target.position.x, target.position.y, transform.position.z);
-
-            if (transitionOver) {
-                
-                Vector2 pos = transform.position;
-                Vector2 targetpos = target.position;
-
-                Vector3 slerped = Vector3.Slerp(pos, targetpos, speed);
-
-                slerped.z = transform.position.z;
-
-                transform.position = slerped;
-            }
-
-            if (camZoomState == CamZoomState.ZoomingIn)
-                {
-                    Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, 2f, 20f * Time.deltaTime);
-                    if (Camera.main.orthographicSize == 2f)
-                    {
-                        camZoomState = CamZoomState.ZoomedIn;
-                    }
-                }
-
-                if (camZoomState == CamZoomState.ZoomingOut)
-                {
-                    Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, DEFAULT_CAMERA_SIZE, 20f * Time.deltaTime);
-                    if (Camera.main.orthographicSize == DEFAULT_CAMERA_SIZE)
-                    {
-                        camZoomState = CamZoomState.ZoomedOut;
-                    }
-                }
+        void Update()
+        {
+            if (partyCameraManager.GetCameraMode() == PartyCameraManagerScriptableObject.CameraMode.Hard_Follow)
         }
+
+        // void Update() {
+        //     _target = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        //     if (transitionOver) {
+                
+        //         Vector2 pos = transform.position;
+        //         Vector2 targetpos = target.position;
+
+        //         Vector3 slerped = Vector3.Slerp(pos, targetpos, partyCameraManager.cameraSpeed);
+
+        //         slerped.z = transform.position.z;
+
+        //         transform.position = slerped;
+        //     }
+
+        //     if (camZoomState == CamZoomState.ZoomingIn)
+        //         {
+        //             Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, 2f, 20f * Time.deltaTime);
+        //             if (Camera.main.orthographicSize == 2f)
+        //             {
+        //                 camZoomState = CamZoomState.ZoomedIn;
+        //             }
+        //         }
+
+        //         if (camZoomState == CamZoomState.ZoomingOut)
+        //         {
+        //             Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, DEFAULT_CAMERA_SIZE, 20f * Time.deltaTime);
+        //             if (Camera.main.orthographicSize == DEFAULT_CAMERA_SIZE)
+        //             {
+        //                 camZoomState = CamZoomState.ZoomedOut;
+        //             }
+        //         }
+        // }
 
         public void PartyLeaderChanged() {
             if (target != null) {
--- Assets/Scripts/Camera/PartyCameraManagerScriptableObject.cs
+++ Assets/Scripts/Camera/PartyCameraManagerScriptableObject.cs
@@ -1,34 +1,123 @@
 using System;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace Manapotion.PartySystem.Cam
 {
+    public enum CameraMode
+    {
+        Hard_Follow,
+        Soft_Follow, 
+        Hard_Move_To_Point,
+        Soft_Move_To_Point,
+        Hard_Cut,
+        Soft_Cut,
+    }
+    
     [CreateAssetMenu(menuName = "Manapotion/ScriptableObjects/New PartyCameraManagerScriptableObject")]
     public class PartyCameraManagerScriptableObject : ScriptableObject
     {
+        private CameraMode _cameraMode; 
+
         public event Action CameraTargetAdded;
+        public event Action CameraTargetRemoved;
+        public event Action<Transform> CameraTargetBiasChanged;
 
         public List<Transform> targets;
+        private bool _canAddTarget = true;
+        private bool _canRemoveTarget = true;
 
-        public Transform AddCameraTarget(Transform target)
+        [SerializeField]
+        private PartyCam partyCamera;
+
+        public float cameraSpeed;
+
+        // returns true if this camera is allowed to have another target added
+        private bool RequestAddTarget(Transform target)
         {
             if (targets == null)
             {
                 targets = new List<Transform>();
             }
+            if (!_canAddTarget)
+            {
+                return false;
+            }
+            else if (targets.Contains(target))
+            {
+                return false;
+            }
+
+            return true;
+        }
+        // returns true if this camera is allowed to have a target removed
+        private bool RequestRemoveTarget(Transform target)
+        {
+            if (targets == null)
+            {
+                targets = new List<Transform>();
+            }
+            if (!_canAddTarget)
+            {
+                return false;
+            }
+            if (!targets.Contains(target))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public IEnumerator AddCameraTarget(Transform target)
+        {
+            if (!RequestAddTarget(target))
+            {
+                yield break;
+            }
 
             targets.Add(target);
             CameraTargetAdded.Invoke();
-            return target;
         }
 
-        public Transform SetCameraTargetBias(Transform biasedTarget = null, int biasedTargetIndex = 0)
+        public IEnumerator RemoveCameraTarget(Transform target)
+        {
+            if (!RequestRemoveTarget(target))
+            {
+                yield break;
+            }
+
+            targets.Remove(target);
+            CameraTargetRemoved.Invoke();
+        }
+
+        public IEnumerator SetCameraTargetBias(Transform biasedTarget = null, int biasedTargetIndex = 0)
         {
             // first check if the target transform is a part of the target list of if the target index is within the target list bounds
             // then make the camera follow the biased target more strongly than the rest
 
             throw new NotImplementedException();
+        }
+
+        public void GetCameraPosition(out Vector3 cameraPosition)
+        {
+            cameraPosition = partyCamera.transform.position;
+        }
+
+        public PartyCam GetPartyCam()
+        {
+            return partyCamera;
+        }
+
+        public void SetCameraMode(CameraMode mode)
+        {
+            _cameraMode = mode;
+        }
+
+        public CameraMode GetCameraMode()
+        {
+            return _cameraMode;
         }
     }
 }
